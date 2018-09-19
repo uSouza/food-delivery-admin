dc0cc19256c55ade0b07e95c94af97693356a6bf
Uesley Souza <uesleysouza97@gmail.com>
2018-09-18 23:10:24 -0300

    Realizado correções de conflitos

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { async, ComponentFixture, TestBed } from '@angular/core/testing'
 import { APP_BASE_HREF } from '@angular/common'
 
@@ -30,32 +29,3 @@ describe('AppComponent', () => {
     expect(component).toBeTruthy()
   })
 })
-=======
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'pandeco-admin'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('pandeco-admin');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pandeco-admin!');
-  }));
-});
->>>>>>> 8124e4285a7d208b95bb1cfcf00c5a6ae0e983b8
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,15 +11,4 @@ export class AppComponent implements OnInit {
 
     ngOnInit() {
     }
-=======
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'pandeco-admin';
->>>>>>> 8124e4285a7d208b95bb1cfcf00c5a6ae0e983b8
 }
