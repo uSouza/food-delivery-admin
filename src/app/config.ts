2527f179884d0e67972850411bdb720ae993c00a
Uesley S. Silva <uesleysilva@alunos.utfpr.edu.br>
2019-06-05 00:00:19 -0300

    Removido configurações privadas

--- src/app/config.ts
+++ src/app/config.ts
@@ -5,13 +5,13 @@ import { Injectable } from '@angular/core';
 export class Config {
 }
 
-export const sender_id = '825111433849';
-export const oneSignalAppId = '18e4fb1f-4d47-4196-8ded-4883a763d9d7';
+export const sender_id = '';
+export const oneSignalAppId = '';
 export const client_id = 1;
-export const client_secret = 'kss5gCVZ2wQyRuwf5AvfPbGbwo4AjCJ8DsXidza8';
+export const client_secret = '';
 
 // Production
-export const url_api = 'http://api.pandeco.com.br/';
+// export const url_api = '';
 
 // Test
-// export const url_api = 'http://localhost:8000/';
+export const url_api = 'http://localhost:8000/';
